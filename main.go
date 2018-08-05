0329d98877a7b5912c655147c39c28efb5792a8f
server4001 <server4001@gmail.com>
2018-08-04 21:59:41 -0700

    Changes data type of --file argument from string to []string

--- main.go
+++ main.go
@@ -6,14 +6,33 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return fmt.Sprintf("%v", *i)
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+
+	return nil
+}
+
+func (i *arrayFlags) Type() string {
+	return ""
+}
+
 var (
-	file string
+	file arrayFlags
 )
 
 func init() {
-	flag.StringVarP(&file, "file", "f", "", "Log files to tail")
+	flag.VarP(&file, "file", "f", "Log files to tail.")
 }
 
 func main() {
+	flag.Parse()
+
 	fmt.Println("Test")
+	fmt.Println(file)
 }
