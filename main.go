f94c83b36bb24ccf78cab8138e1345d4f2cf5d91
server4001 <server4001@gmail.com>
2018-08-14 12:59:04 -0700

    Changes CLI flags for a JSON config file

--- main.go
+++ main.go
@@ -1,25 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	flag "github.com/spf13/pflag"
+	"io/ioutil"
 )
 
-var (
-	file  logFilesFlag
-	regex string
-)
+func getConfig() []Watcher {
+	// TODO : CHANGE THIS.
+	configFilePath := "/Users/briceb/projects/go/src/github.com/server4001/logfile-alert/config.json"
 
-func init() {
-	flag.VarP(&file, "file", "f", "Log files to tail.")
-	flag.StringVarP(&regex, "regex", "r", "", "Regex to look for in log files.")
+	configRaw, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	var configParsed map[string]interface{}
+
+	jsonErr := json.Unmarshal(configRaw, &configParsed)
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+
+	watchersParsed := configParsed["watchers"].([]interface{})
+	watchers := make([]Watcher, 0)
+
+	for idx := range watchersParsed {
+		watcher := watchersParsed[idx].(map[string]interface{})
+		logFiles := watcher["log_files"].([]interface{})
+		regex := watcher["regex"].(string)
+
+		watchers = append(watchers, Watcher{
+			logFiles,
+			regex,
+		})
+	}
+
+	return watchers
 }
 
 func main() {
-	flag.Parse()
+	watchers := getConfig()
 
-	fmt.Println("Test")
-	fmt.Println(file)
-	fmt.Println(regex)
+	for _, watcher := range watchers {
+		fmt.Println(watcher.regex)
+		for _, logFile := range watcher.logFiles {
+			fmt.Println(logFile)
+		}
+	}
+
+	// sigs := make(chan os.Signal, 1)
+	// done := make(chan bool, 1)
+
 }
---
+++ watcher.go
@@ -0,0 +1,6 @@
+package main
+
+type Watcher struct {
+	logFiles []interface{}
+	regex    string
+}
